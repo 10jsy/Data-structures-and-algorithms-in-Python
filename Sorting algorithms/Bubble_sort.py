88ada619632a036bbcb8efb3db243f14270e37c4
Jea Seong Yoon <jeaseongyoon@Jeas-MacBook-Pro.local>
2020-04-25 08:45:50 +0100

    Added bubble sort

---
+++ Sorting algorithms/Bubble_sort.py
@@ -0,0 +1,9 @@
+def bubble_sort(alist):
+    for num in range(len(alist)-1, 0, -1):
+        for i in range(num):
+            if alist[i] > alist[i+1]:
+                alist[i], alist[i+1] = alist[i+1], alist[i] 
+
+sort_list = [5,8,1,3,4,88,11,3,15,666,7,18,81,444]
+bubble_sort(sort_list)
+print(sort_list)
