971c9d9a907472b888c07037eb2d51b41ed614ac
Jea Seong Yoon <jeaseongyoon@Jeas-MacBook-Pro.local>
2020-04-25 06:29:19 +0100

    Added sequential search

--- Searches/Sequential_search.py
+++ Searches/Sequential_search.py
@@ -12,5 +12,20 @@ def sequential_search(alist, item):
             pos += 1
     return found
 
+def ordered_sequential_search(orderedlist, item):
+    pos = 0
+    found = False
+
+    while pos < len(orderedlist) and not found:
+        if orderedlist[pos] == item:
+            return True
+        elif orderedlist[pos] > item:
+            return False
+        else:
+            pos += 1
+    return found
+
 test_list = [11,5,7,23,4,2,6,4,63,7,4,44]
+test_ordered_list = [1,2,3,4,5,6,7,9,10,11,12]
 print(sequential_search(test_list, 11))
+print(ordered_sequential_search(test_ordered_list, 8))
